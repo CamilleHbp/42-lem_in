a05216e28c84d44cf184d28aa44485c1943ccd1c
Camille Baillat <camille.baillat@protonmail.com>
2018-02-22 18:29:11 +0100

    tentative de description de l'algo dans overwatch.c

--- srcs/algorithm/overwatch.c
+++ srcs/algorithm/overwatch.c
@@ -6,7 +6,7 @@
 /*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 13:00:09 by cbaillat          #+#    #+#             */
-/*   Updated: 2018/02/22 16:37:58 by cbaillat         ###   ########.fr       */
+/*   Updated: 2018/02/22 18:28:52 by cbaillat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,37 @@
 
 /* Logic:
 **
-** Dans cet exemple il y a deux chemins max. Sinon on lance une recursive par
-** chenmin max possible.
+** WARNING: cette technique risque de prendre un temps extremement long si
+** les chemins possibles sont nombreux. Peut etre prevoir un nombre de recursions
+** maximum ?
+**
+** Dans cet exemple il y a trois chemins max. Sinon on lance une recursive par
+** chemin max possible.
+**
+** On fait un tableau en 2d.
+** way[0] => meilleur chemin pour 1 seul chemin
+** way[1] => tableau des 2 meilleur chemins possibles qui fonctionnent ensemble
+** way[2] => tableau des 3 meilleur chemins possibles qui fonctionnent ensemble
+**
+** On lance la recursive pour recuperer le meilleur chemin. Si on ne trouve pas,
+** la carte est fausse, on imprime 'error'.
+**
+** Si on trouve le chemin le plus rapide, on revient dans la fonction principale
+** On copie le chemin le plus rapide dans l'index way[1][0] et on lance la
+** recursive pour trouver un autre chemin vers END.
+** Si on trouve c'est cool, sinon on backtrack sur le premier way et on relance
+** la recursive. Ainsi de suite. Si on ne trouve rien alors le seul chemin possibl
+** est celui sauvegarde dans l'index way[0].
+**
+** On procede de meme
+**
+** CE QUI SUIT EST OBSOLETE UN EXEMPLE DE CE QUI POURRAIT SE PASSER DANS LE
+** BACKTRACK
 **
 ** F1:
-** On lance rec 1 et on sauve dans WA-1.
+** On a deja le chemin le plus court dans way[1][0]
 ** F2:
-** On lance rec 2 et on sauve dans WB-1.
+** On lance rec 2 et on sauve dans way[1][1].
 **
 ** Si F2 retourne FALSE, on relance F1 avec une copie de WA-1 appelee WA-2.
 ** WA-2 est une copie de WA-1 avec ces differences:
